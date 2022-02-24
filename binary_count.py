e96c27a850cdac75491d22f658748eeb0b9faee2
Alex Sage <alexander.p.sage@gmail.com>
2022-02-24 13:03:41 -0700

    both run correctly. should do a profile

--- binary_count.py
+++ binary_count.py
@@ -1,35 +1,122 @@
+from typing import List
+import numpy as np
+import time
+
+
+ADD = 'ADD'
+COUNT = 'COUNT'
 challenge_prompt = ("""
 Challenge: 
     input: List[char]
             a list of single char strings and a binary number 
-            represented as 1s and 0s in a str. the char can be 'count' or 'add'. 
-    algorithm: for every 'count' count the number of ones in the binary number. The len of the digits in the number must stay constant. 
-            It must run quickly. every 'add' means add 1 to the number.
+            represented as 1s and 0s in a str. the char can be 'COUNT' or 'ADD'. 
+    algorithm: for every 'COUNT' COUNT the number of ones in the binary number. The len of the digits in the number must stay constant. 
+            It must run quickly. every 'ADD' means ADD 1 to the number.
     return: List[int]
-            list of ints where each is a response to each 'count'. The length of the return list will be the same length as the number of 'count'
-            in the request list, not necessarily the same length of the request list""")
-print(challenge_prompt)
-import numpy as np
+            list of ints where each is a response to each 'COUNT'. The length of the return list will be the same length as the number of 'COUNT'
+            in the request list, not necessarily the same length of the request list\n""")
 
-def solve_as_str(request, bin_str):
-    print(request, bin_str)
+print(challenge_prompt)
+
+
+def count_ones(bin_str: str) -> int:
+    ct = 0
+    for val in bin_str:
+        if val=='1':
+            ct +=1
+    return ct
+    
+def solve_as_bin(requests: List[str], bin_str: str, verbose=False) -> List[int]:
+    bin_str_orig = bin_str
+    ret = []
+    len_str = len(bin_str)
+    modu = pow(2,len_str)
+    for req in requests:
+        if req==ADD:
+            dec = int(bin_str, 2)
+            if verbose:
+                print("add")
+                print(bin_str, dec)
+            bin_str = format((dec+1)%modu, 'b').zfill(len_str)
+            if verbose:
+                print(bin_str)
+
+        elif req==COUNT:
+            ct = count_ones(bin_str)
+            if verbose:
+                print(bin_str, 'count', ct)
+            ret.append(ct)
+        else:
+            raise ValueError("only ADD and COUNT are acceptable request types")
+    assert len(bin_str) == len(bin_str_orig), "size of binary cannot change"
+    return ret
+
+def solve_as_str(requests: List[str], bin_str: str, verbose=False) -> List[int]:
+    bin_str_orig = bin_str
+    ret = []
+    for req in requests:
+        if req==ADD:
+            bits = list(bin_str)
+            if verbose:
+                print("add")
+                print(bin_str)
+            set_one = False
+            for i in range(len(bits)):
+                if bits[-i-1] =='1':
+                    bits[-i-1] = '0'
+                elif bits[-i-1] =='0':
+                    bits[-i-1] = '1'
+                    set_one = True
+                    break
+                else:
+                    raise ValueError("only allows values 0 or 1 in binary")
+            if not set_one:
+                bits[-1] = '1'
+            bin_str = ''.join(bits)
+            if verbose:
+                print(bin_str, "\n")
+        elif req==COUNT:
+            ct = count_ones(bin_str)
+            if verbose:
+                print(bin_str, 'count', ct)
+
+            ret.append(ct)
+        else:
+            raise ValueError("only ADD and COUNT are acceptable request types")
+    assert len(bin_str) == len(bin_str_orig), "size of binary cannot change"
+    return ret
+
+def random_data():
+    request_size = np.random.randint(1,100)
+    binary_size =  np.random.randint(1,100)
+    requests = []
+    bits = ['0' for x in range(binary_size)]
+    for j in range(binary_size):
+        if 0.5 < np.random.rand():
+            bits[j] = '1'
+    for j in range(request_size):
+        if 0.5 < np.random.rand():
+            requests.append(COUNT)
+        else:
+            requests.append(ADD)
+    return requests, ''.join(bits)
+
 
 if __name__=="__main__":
-    for i in range(10):
-        request_size = np.random.randint(1,10)
-        binary_size =  np.random.randint(1,10)
-        request = []
-        bin_str = ['0' for x in range(binary_size)]
-        for j in range(binary_size):
-            if 0.5 < np.random.rand():
-                bin_str[j] = '1'
-        for j in range(request_size):
-            if 0.5 < np.random.rand():
-                request.append('count')
-            else:
-                request.append('add')
+    test_dataset = []
+    for i in range(50):
+        test_dataset.append( random_data())
+    start = time.perf_counter()
+    for requests, bin_str in test_dataset:
+        solve_as_str(requests, bin_str)
+    end = time.perf_counter()
+    print("time to run solve as str", (end - start))
 
-        solve_as_str(request, ''.join(bin_str))
+    start = time.perf_counter()
+    for requests, bin_str in test_dataset:
+        solve_as_bin(requests, bin_str)
+    end = time.perf_counter()
+    print("time to run solve as str", (end - start))
 
 
 
