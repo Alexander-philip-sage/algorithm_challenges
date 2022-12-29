0db7c6bb9af23247ecf7accb6b0756dfe03ea3ee
sage <alexander.philip.sage@gmail.com>
2022-12-28 17:00:15 -0700

    cleaning and testing

--- door_entry.py
+++ door_entry.py
@@ -4,10 +4,51 @@ input: List[List[str, str]]
     List of events. Event is a list of two strings. first string is the person's name, second is direction they pass through the door "in" or "out".
 
 algorithm: 
-    A buglary support system tracking customer's faces tracks which direction the customer walks through the door to ensure that everyone enters and exits through the main door during business hours, not before, not after. 
+    A buglary support system tracking customer's faces tracks which direction the customer walks through the door to ensure that everyone enters and 
+    exits through the main door during business hours, not before, not after. 
 
 return: bool
     whether or not the system detected an error."""
+
+
+
+def track(people_tic: Dict[str, int], event: List[str]) -> bool:
+    '''track if the action should set off an alarm. if not, log the action
+    input: people_tic: a dictionary where key is the person's name/id and the value is the counter of how they have moved through the front door.
+    event: is a list of inputs [person's name, in/out]'''
+    if event[1]=='in':
+        if people_tic[event[0]] ==0: 
+            people_tic[event[0]] +=1
+            return True
+        else:
+            return False
+    elif event[1]=='out':
+        if people_tic[event[0]] ==1:
+            people_tic[event[0]] -=1
+            return True
+        else:
+            return False
+    else:
+        raise ValueError("only accepted events are [in, out], did not recognize: "+str(event[1]))
+    
+
+def detect(events: List[List[str]]) -> bool:
+    '''given list of events, detect if an alarm should fire based on incorrect use of the front door
+    input: List of events where each event is a list of inputs [person's name, in/out]'''
+    people_tic = {}
+    for ev in events:
+        if ev[0] in people_tic.keys():
+            if not track(people_tic, ev):
+                return False
+        else:
+            if ev[1]=='in':
+                people_tic[ev[0]] = 1
+            else:
+                return False
+    if sum(list(people_tic.values())) ==0:
+        return True
+    else:
+        return False
 
 def tests():
     cases = [
@@ -45,40 +86,5 @@ def tests():
             print(events)
     if passed:
         print("All tests passed")
-
-def track(people_tic: Dict[str, int], event: List[str]) -> bool:
-    if event[1]=='in':
-        if people_tic[event[0]] ==0: 
-            people_tic[event[0]] +=1
-            return True
-        else:
-            return False
-    elif event[1]=='out':
-        if people_tic[event[0]] ==1:
-            people_tic[event[0]] -=1
-            return True
-        else:
-            return False
-    else:
-        return False
-    
-
-def detect(events: List[List[str]]) -> bool:
-    people_tic = {}
-    for ev in events:
-        if ev[0] in people_tic.keys():
-            bl = track(people_tic, ev)
-            if not bl:
-                return False
-        else:
-            if ev[1]=='in':
-                people_tic[ev[0]] = 1
-            else:
-                return False
-    if sum(list(people_tic.values())) ==0:
-        return True
-    else:
-        return False
-
 if __name__=="__main__":
     tests()
