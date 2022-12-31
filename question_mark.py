da51f0043c1bc9a90ac53fa3f672ba65c7e0a70b
sage <alexander.philip.sage@gmail.com>
2022-12-30 20:29:59 -0700

    2x speedup

--- question_mark.py
+++ question_mark.py
@@ -1,3 +1,4 @@
+import datetime
 '''Take an input string parameter and determine if exactly 3 question marks exist between every pair of numbers 
 that add up to 10. If so, return True, otherwise return False. Some examples test cases are below'''
 '''its unclear if negative numbers or floats are allowed inputs. assume not
@@ -5,11 +6,41 @@ assuming only single digits are considered'''
 test_cases = [("arrb6???4xxbl5???eee5",True),
             ("acc?7??sss?3rr1??????5", True),
             ("5??aaaaaaaaaaaaaaaaaaa?5?5", False),
+            ("5??aaaaaaaaaaaaaaaaaaa?55", False),
+            ("7??aaaaaaaaaaaaaaaaaaa?33", True),
             ("9???1???9???1???9" , True),
             ("???30?10?0?07" , True),
             ("???30?10?0?07???3???" , True),
-            ("aa6?9", False)]
+            ("aa6?9", False),
+            ("aa6??7?4??39", True),
+            ("aa69", False),
+            ("1937", False),
+            ("???", False),
+            ("aa", False),
+            ("".join(['5a?' for x in range(100000000)]),False)                        
+            ]
 
+def question_mark2(var: str) -> bool:
+    '''input: var : a string with some number of question marks, letters and numbers
+    return: a boolean indicating if there are three question marks between every 
+    adjacent pair of digits that add to 10
+    algorithm: this is the second version. it runs in O(n) time'''
+    digit_lookup = {}
+    ct_qs = 0
+    res = False
+    for chr in var:
+        if chr=='?':
+            ct_qs+=1
+        elif chr.isdigit():
+            dchr = int(chr)
+            compl = 10-dchr
+            if compl in digit_lookup.keys():
+                if (ct_qs-digit_lookup[compl])==3:
+                    res = True
+                else:
+                    return False
+            digit_lookup[dchr] = ct_qs
+    return res
 def question_mark(var):
     questions = []
     numbers = []
@@ -41,20 +72,31 @@ def question_mark(var):
         return True
     else:
         return False
-for test in test_cases:
-    resp=question_mark(test[0])
-    print(test[0])
-    if resp ==test[1]:
-        print("correct\n")
-    else:
-        print("wrong")
-        print("expected", test[1])
-        print("returned", resp)
-        print()
-
+def test_func(func):
+    print()
+    print("testing", func.__name__)
+    start = datetime.datetime.now()
+    failed = False
+    for i in range(1000000):
+        func(test_cases[-1][0])
+    for test in test_cases:
+        resp=func(test[0])
+        if resp!=test[1]:
+            failed = True
+            print()
+            print(test[0])
+            print("Failed")
+            print("expected", test[1])
+            print("returned", resp)
+    if not failed:
+        print("all tests passed")
+    print("processing time", (datetime.datetime.now()-start).total_seconds())
+test_func(question_mark)
+test_func(question_mark2)
 '''the question is extremely poorly stated for a written question where the user can't ask questions. 
 it says every pair of numbers that add up to 10, but that isn't correct. It means to say count all the
-question marks between a digit and its closest digit that sums to equal 10. 
+question marks between a digit and its closest digit that sums to equal 10. this sum of question marks should equal 3.
+if it were every pair that added to 10 then you couldn't have more than two fives in the same string.
 the instruction is also unclear about what to do in the edge case of zero pairs found. these details have to be 
 gleaned from the test cases.
 this could probably be done faster by storing the numbers in a dict instead of an array where every key was the digit and 
